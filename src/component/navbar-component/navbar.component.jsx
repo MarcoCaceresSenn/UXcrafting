027cbfdb409dc3d3ebe35847598434642835c433
MarcoCaceresSenn <m.caceres04@ufromail.cl>
2023-10-28 09:17:33 -0300

    integracion navbar responsive

--- src/component/navbar-component/navbar.component.jsx
+++ src/component/navbar-component/navbar.component.jsx
@@ -1,24 +1,26 @@
 import React from 'react';
-//import Logo from '../../assets/images/logo-blanco.svg';
 import Container from 'react-bootstrap/Container';
 import Nav from 'react-bootstrap/Nav';
 import Navbar from 'react-bootstrap/Navbar';
 import './navbar.component.css';
 import CustomNavbarBrand from './custom.navbar-brand.jsx';
-import 'bootstrap/dist/css/bootstrap.min.css'
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function NavBarComponent() {
     return (
         <>
-            <Navbar className='navbar bg-bcp' data-bs-theme="dark">
+            <Navbar className='navbar bg-bcp' expand="lg" variant="dark">
                 <Container>
                     <CustomNavbarBrand />
-                    <Nav className="ml-auto align-items-center gap-5">
-                        <Nav.Link href="#">Inicio</Nav.Link>
-                        <Nav.Link href="#">Operaciones</Nav.Link>
-                        <Nav.Link className="custom-feature rounded" href="/">Mis Finanzas</Nav.Link>
-                        <Nav.Link href="#">Explora</Nav.Link>
-                    </Nav>
+                    <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                    <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
+                        <Nav className="ml-auto align-items-center gap-5">
+                            <Nav.Link href="#">Inicio</Nav.Link>
+                            <Nav.Link href="#">Operaciones</Nav.Link>
+                            <Nav.Link className="custom-feature rounded" href="/">Mis Finanzas</Nav.Link>
+                            <Nav.Link href="#">Explora</Nav.Link>
+                        </Nav>
+                    </Navbar.Collapse>
                 </Container>
             </Navbar>
         </>
