49b18f0501317310055109364689bfaa9e82af7e
Tootie <c.negroni01@ufromail.cl>
2023-10-28 07:29:25 -0300

    pagina inicial terminada

--- src/component/navbar-component/custom.navbar-brand.jsx
+++ src/component/navbar-component/custom.navbar-brand.jsx
@@ -3,7 +3,7 @@ import Logo from '../../assets/images/logo-blanco.svg'
 import Navbar from 'react-bootstrap/Navbar';
 export default function CustomNavbarBrand() {
     return (
-        <Navbar.Brand href="#home" className="d-flex align-items-center">
+        <Navbar.Brand href="/" className="d-flex align-items-center">
             <img
                 src={Logo}
                 alt="Logo blanco BCP"
--- src/component/progressbar-component/progressbar.component.jsx
+++ src/component/progressbar-component/progressbar.component.jsx
@@ -1,15 +1,34 @@
-import React from 'react';
-import ProgressBar from 'react-bootstrap/ProgressBar';
-import './progressbar.component.css';
+import React from "react";
+import ProgressBar from "react-bootstrap/ProgressBar";
+import "./progressbar.component.css";
+import Button from "../button-component/button.component.jsx";
 
 export default function Progressbar({ mesSeleccionado, ingreso, gasto }) {
   const valorPorDefecto = 0;
-  const porcentajeIngreso = (ingreso - gasto) / ingreso * 100;
+  const porcentajeIngreso = ((ingreso - gasto) / ingreso) * 100;
   const valor = isNaN(porcentajeIngreso) ? valorPorDefecto : porcentajeIngreso;
 
+  console.log(valor);
+
   return (
-    <div className="d-flex justify-content-center align-items-center" style={{ height: '15vh' }}>
-      <ProgressBar className='w-50' now={valor} label={`${valor.toFixed(2)}%`} />
+    <div>
+      <div className="d-flex justify-content-center">
+      <p className="gastos">Gastos</p>
+      <p className="ingreso">Ingresos</p>
+      </div>
+      <div
+        className="d-flex justify-content-center align-items-center"
+        style={{ height: "5vh" }}  
+      >
+   
+        <ProgressBar
+          className="w-50"
+          now={valor}
+          label={`${valor.toFixed(2)}%`}
+        />
+         
+      </div>
+      <Button porcentaje={valor} />
     </div>
   );
 }
