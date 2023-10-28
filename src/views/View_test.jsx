f5fad683f04ab14a7141cedf650caa4552b03e33
Tootie <c.negroni01@ufromail.cl>
2023-10-27 22:48:12 -0300

    coreccion grafico

--- src/views/View_test.jsx
+++ src/views/View_test.jsx
@@ -1,6 +1,6 @@
 //import Sidebar from '../component/sidebar-component/sidebar.component'
 //import Progressbar from '../component/progressbar-component/progressbar.component.jsx'
-import Graph from '../component/graph-component/LinesChart.jsx'
+import Graph from '../component/graph-component/BarChart.jsx'
 export default function View_test() {
     return (
         <div>
