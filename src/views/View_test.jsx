0a064582c18b8886d721cc3577995fe509312b4e
Tootie <c.negroni01@ufromail.cl>
2023-10-27 22:44:04 -0300

    Graficos

--- src/views/View_test.jsx
+++ src/views/View_test.jsx
@@ -1,11 +1,10 @@
-import Sidebar from '../component/sidebar-component/sidebar.component'
-import Progressbar from '../component/progressbar-component/progressbar.component.jsx'
-
+//import Sidebar from '../component/sidebar-component/sidebar.component'
+//import Progressbar from '../component/progressbar-component/progressbar.component.jsx'
+import Graph from '../component/graph-component/LinesChart.jsx'
 export default function View_test() {
     return (
         <div>
-            <Sidebar/>
-            <Progressbar/>
+            <Graph/>
         </div>
     )
 }
